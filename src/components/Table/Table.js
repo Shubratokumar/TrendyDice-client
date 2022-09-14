f5b2a275024e066f22ada333e2be657b43628303
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-09-14 12:43:41 +0600

    pagination done

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { Button, styled } from '@mui/material';
+import { Button,Box, FormControl, Select, MenuItem,ToggleButton,  styled} from '@mui/material';
 import "../../styles/table.css";
+import axios  from "axios";
+// import { useQuery } from 'react-query';
 
 const ActionBtn = styled(Button)`
     border: 1px solid white;
@@ -12,19 +14,41 @@ const ActionBtn = styled(Button)`
     font-weight: 500;
     color: white;
 `;
+const PaginationWrapper = styled(Box)`
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  margin: 10px;
+  padding: 10px 15px;
+`;
+const BtnWrapper = styled(ToggleButton)`
+    margin: 3px;
+`
 
 const Table = () => {
   const [studentsData, setStudentsData] = useState([]);
+  const [ limit, setLimit ] = useState(10);
+  const [ pageNumber, setPageNumber ] = useState(0);
+  const [ totalPage, setTotalPage ] = useState(0);
+  const [selected, setSelected] = useState(false);
 
-  useEffect(() => {
-    const url = `https://shrouded-savannah-90405.herokuapp.com/student`;
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => setStudentsData(data));
-  }, []);
+
+  /* const {
+    data : studentsData,
+    refetch,
+    } =  useQuery("student", () =>
+    fetch(`http://localhost:5000/student?limit=10`).then((res) => res.json())
+    );  */
+  useEffect(() => {    
+    ( async () => {
+        const { data } = await axios.get(`http://localhost:5000/student?limit=${limit}&pageNumber=${pageNumber}`);
+        setStudentsData(data?.data);
+        setTotalPage(data?.count/limit);
+
+      })() 
+  }, [limit, pageNumber]);
   
   const handlePayment = (id) => {
-    console.log(id);
     const url = `https://shrouded-savannah-90405.herokuapp.com/student/${id}`;
     fetch(url, {
       method: "PUT",
@@ -38,7 +62,7 @@ const Table = () => {
       .then((res) => res.json())
       .then((data) => {
         if (data?.modifiedCount > 0) {
-          console.log("Student payment data updated.")
+          console.log("Student payment data updated.");
         }
       });
   }
@@ -79,6 +103,34 @@ const Table = () => {
           }        
         </tbody>
       </table>
+      <PaginationWrapper>
+        {
+          [...Array(totalPage).keys()].map(number => {
+              return (
+                <BtnWrapper
+                  value= "check"
+                  selected={ pageNumber === number ?  !selected : selected }
+                  
+                  onClick={() => setPageNumber(number)}
+                >
+                  { number+1 }
+                </BtnWrapper>
+          )})
+        }
+            <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+              <Select  
+                value={limit}
+                onChange={(e) => setLimit(e.target.value)}
+              >
+                <MenuItem value={10}>10</MenuItem>
+                <MenuItem value={15}>15</MenuItem>
+                <MenuItem value={20}>20</MenuItem>
+                <MenuItem value={30}>30</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+      </PaginationWrapper>
     </>
   );
 };
